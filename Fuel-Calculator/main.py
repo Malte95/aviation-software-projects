0d0be436d7763544f19ddddc668c7d136c9793d6
Malte95 <maltescherenberg@gmx.de>
2026-05-27 11:39:45 +0200

    Improve fuel calculation and input validation

--- Fuel-Calculator/main.py
+++ Fuel-Calculator/main.py
@@ -3,20 +3,15 @@ distance = float(input("Enter distance:"))
 wind_strength = float(input("Enter wind strength:"))
 wind_direction = input("Headwind or tailwind: ")
 
-
-if wind_direction.lower() == "headwind":
-    print("Consumption increases")
-elif wind_direction.lower() == "tailwind":
-    print("Consumption decreases")
-else:
-    print("Invalid input")
-
 def calculate_fuel(weight, distance, wind_strength, wind_direction):
     base_consumption_per_km = 5
-    consumption = base_consumption_per_km * distance
+    weight_factor = 0.0001
+    scaled_weight = weight / 100
+    consumption = base_consumption_per_km * distance * ( 1 + scaled_weight * weight_factor)
     return consumption
 
-fuel_consumption = calculate_fuel(weight, distance, wind_strength, wind_direction)
-
-
-print(f"Weight: {weight} | Distance: {distance} | Wind strength: {wind_strength} | Wind direction: {wind_direction} | Fuel consumption: {fuel_consumption}")
+if wind_direction.lower() not in ["headwind", "tailwind"]:
+    print("Invalid input")
+else:
+    fuel_consumption = calculate_fuel(weight, distance, wind_strength, wind_direction)
+    print(f"Weight: {weight} | Distance: {distance} | Wind strength: {wind_strength} | Wind direction: {wind_direction} | Fuel consumption: {fuel_consumption:.2f} L")
