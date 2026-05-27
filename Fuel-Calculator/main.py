e53287b24613f1f6f916ed595aa4300d45c483ff
Malte95 <maltescherenberg@gmx.de>
2026-05-27 10:57:06 +0200

    Add Fuel-Calculator input and wind logic

--- Fuel-Calculator/main.py
+++ Fuel-Calculator/main.py
@@ -1,1 +1,13 @@
+weight = float(input("Enter weight:"))
+distance = float(input("Enter distance:"))
+wind_strength = float(input("Enter wind strength:"))
+wind_direction = input("Headwind or tailwind: ")
 
+if wind_direction.lower() == "headwind":
+    print("Consumption increases")
+elif wind_direction.lower() == "tailwind":
+    print("Consumption decreases")
+else:
+    print("Invalid input")
+
+print(f"Weight: {weight} | Distance: {distance} | Wind strength: {wind_strength} | Wind direction: {wind_direction}")
