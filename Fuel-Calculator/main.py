2601be4d424421b41effd0cf2944034880c95a89
Malte95 <maltescherenberg@gmx.de>
2026-05-28 13:00:41 +0200

    Add input validation for fuel calculator

--- Fuel-Calculator/main.py
+++ Fuel-Calculator/main.py
@@ -1,23 +1,32 @@
-weight = float(input("Enter weight:"))
-distance = float(input("Enter distance:"))
-wind_strength = float(input("Enter wind strength:"))
-wind_direction = input("Headwind or tailwind: ")
+try:
+    weight = float(input("Enter weight:"))
+    distance = float(input("Enter distance:"))
+    wind_strength = float(input("Enter wind strength:"))
+    wind_direction = input("Headwind or tailwind: ")
 
-def calculate_fuel(weight, distance, wind_strength, wind_direction):
-    base_consumption_per_km = 5
-    weight_factor = 0.0001
-    scaled_weight = weight / 100
-    wind_factor = 0.001
-    wind_effect = wind_strength * wind_factor
-    consumption = base_consumption_per_km * distance * ( 1 + scaled_weight * weight_factor)
-    if wind_direction.lower() == "headwind":
-        consumption *= 1 + wind_effect
-    elif wind_direction.lower() == "tailwind":
-        consumption *= 1 - wind_effect
-    return consumption
+    if weight < 0 or distance < 0 or wind_strength < 0:
+        print("Please enter a positive number")
+    else:
 
-if wind_direction.lower() not in ["headwind", "tailwind"]:
-    print("Invalid input")
-else:
-    fuel_consumption = calculate_fuel(weight, distance, wind_strength, wind_direction)
-    print(f"Weight: {weight} | Distance: {distance} | Wind strength: {wind_strength} | Wind direction: {wind_direction} | Fuel consumption: {fuel_consumption:.2f} L")
+        def calculate_fuel(weight, distance, wind_strength, wind_direction):
+            base_consumption_per_km = 5
+            weight_factor = 0.0001
+            scaled_weight = weight / 100
+            wind_factor = 0.001
+            wind_effect = wind_strength * wind_factor
+            consumption = base_consumption_per_km * distance * ( 1 + scaled_weight * weight_factor)
+            if wind_direction.lower() == "headwind":
+                consumption *= 1 + wind_effect
+            elif wind_direction.lower() == "tailwind":
+                consumption *= 1 - wind_effect
+            return consumption
+
+        if wind_direction.lower() not in ["headwind", "tailwind"]:
+            print("Invalid input")
+        else:
+            fuel_consumption = calculate_fuel(weight, distance, wind_strength, wind_direction)
+            print(f"Weight: {weight} | Distance: {distance} | Wind strength: {wind_strength} | Wind direction: {wind_direction} | Fuel consumption: {fuel_consumption:.2f} L")
+except ValueError:
+    print("Please enter numeric values only")
+
+
